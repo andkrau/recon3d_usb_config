1288bf7088dde9c41e46bbbfa6ae120294ef95da
andkrau <23638596+andkrau@users.noreply.github.com>
2025-10-11 19:18:55 -0500

    Use detected language strings to match to hardware instance ID

--- configure.cmd
+++ configure.cmd
@@ -40,19 +40,34 @@ IF "%CAPTURE_FOUND%" EQU "FALSE" GOTO CAPTURE_NOT_DETECTED
 GOTO SUCCESS
 
 :CHECK_ENDPOINTS
-IF "%~1" EQU "Instance ID" for /f "tokens=3 delims=\" %%a in ("%~2") do set ID=%%a
-IF "%~1" EQU "Device Description" for /f "tokens=*" %%a in ("%~2") do set DESCRIPTION=%%a
-IF "%~1" EQU "Class Name" (
-  IF "%DESCRIPTION%" EQU "Speaker (Sound Blaster Recon3D)" (
+SET "VALUE=%~2"
+IF NOT "%VALUE:MMDEVAPI=%" EQU "%VALUE%" SET COUNT=1
+IF %COUNT% EQU 1 for /f "tokens=3 delims=\" %%m in ("%~2") do set "ID=%%m"
+IF %COUNT% EQU 2 for /f "tokens=*" %%m in ("%~2") do set "DESCRIPTION=%%m"
+IF %COUNT% EQU 3 (
+  IF "%DESCRIPTION%" EQU "%SPEAKER% (%MIXER%)" (
     SET RENDER_FOUND=TRUE
     REG ADD "HKCU\SOFTWARE\Creative Tech\Audio Endpoint Selection\Sound Blaster Recon 3D Control Panel" /v Render /t REG_SZ /d "%ID%" /f >nul
   )
-  IF "%DESCRIPTION%" EQU "Microphone (Sound Blaster Recon3D)" (
+  IF "%DESCRIPTION%" EQU "%MICROPHONE% (%MIXER%)" (
     SET CAPTURE_FOUND=TRUE
     REG ADD "HKCU\SOFTWARE\Creative Tech\Audio Endpoint Selection\Sound Blaster Recon 3D Control Panel" /v Capture /t REG_SZ /d "%ID%" /f >nul
   )
   SET DESCRIPTION=""
   SET ID=""
+)
+SET /a COUNT+=1
+GOTO :EOF
+
+:ASSIGN_STRING
+reg query %1 /v %2 >nul 2>&1
+if %errorlevel% NEQ 0 goto REG_MISSING
+for /f "tokens=3*" %%a in ('reg query %1 /v %2 2^>nul') do (
+    if "%%b"=="" (
+        set "%3=%%a"
+    ) else (
+        set "%3=%%a %%b"
+    )
 )
 GOTO :EOF
 
