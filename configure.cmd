d8c1206a2389446337fe19435db8502a8686d316
andkrau <23638596+andkrau@users.noreply.github.com>
2025-10-11 19:31:01 -0500

    Consistent formatting

--- configure.cmd
+++ configure.cmd
@@ -1,82 +1,82 @@
-@ECHO OFF
-TITLE Recon3D Configuration
-COLOR 9F
-SET RENDER_FOUND=FALSE
-SET CAPTURE_FOUND=FALSE
-SET "LANGUAGE=Default"
-SET "MIXER="
-SET "SPEAKER="
-SET "MICROPHONE="
-SET "BASE=HKEY_LOCAL_MACHINE\SOFTWARE\Creative Tech\KSAud"
-SET COUNT=0
+@echo off
+title Recon3D Configuration
+color 9F
+set RENDER_FOUND=FALSE
+set CAPTURE_FOUND=FALSE
+set "LANGUAGE=Default"
+set "MIXER="
+set "SPEAKER="
+set "MICROPHONE="
+set "BASE=HKEY_LOCAL_MACHINE\SOFTWARE\Creative Tech\KSAud"
+set COUNT=0
 
 :LOGO
 chcp 65001
 cls
-ECHO.
-ECHO.       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓░▓▓▓▓▓▓▓░
-ECHO.      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░       ▓▓  ▓▓   ░▓▓
-ECHO.     ░▓▓▓▓             ░▓▓▓▓       ▓▓▓▓▓ ░▓▓▓▓             ▒▓▓▓▓       ▓▓▓▓▓ ▒▓▓▓▓       ▓▓▓▓▓ ▒▓▓▓▓▓▓▓▒ ▓▓▒   ▓▓
-ECHO.     ▓▓▓▓              ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓              ▓▓▓▓        ▓▓▓▓  ▓▓▓▓        ▓▓▓▓        ▓▓  ▓▓   ▒▓▓
-ECHO.    ▓▓▓▓▒             ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░ ▓▓▓▓▒             ▓▓▓▓▓       ▓▓▓▓░ ▓▓▓▓▒       ▓▓▓▓░ ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓
-ECHO.   ░▓▓▓▓             ▒▓▓▓▓             ▒▓▓▓▓             ▒▓▓▓▓       ▓▓▓▓▓ ▒▓▓▓▓       ▓▓▓▓▓
-ECHO.   ▓▓▓▓              ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓        ▓▓▓▓
-ECHO.  ▓▓▓▓▓              ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▒       ▓▓▓▓░
-ECHO.
+echo.
+echo.       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓░▓▓▓▓▓▓▓░
+echo.      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░       ▓▓  ▓▓   ░▓▓
+echo.     ░▓▓▓▓             ░▓▓▓▓       ▓▓▓▓▓ ░▓▓▓▓             ▒▓▓▓▓       ▓▓▓▓▓ ▒▓▓▓▓       ▓▓▓▓▓ ▒▓▓▓▓▓▓▓▒ ▓▓▒   ▓▓
+echo.     ▓▓▓▓              ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓              ▓▓▓▓        ▓▓▓▓  ▓▓▓▓        ▓▓▓▓        ▓▓  ▓▓   ▒▓▓
+echo.    ▓▓▓▓▒             ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░ ▓▓▓▓▒             ▓▓▓▓▓       ▓▓▓▓░ ▓▓▓▓▒       ▓▓▓▓░ ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓
+echo.   ░▓▓▓▓             ▒▓▓▓▓             ▒▓▓▓▓             ▒▓▓▓▓       ▓▓▓▓▓ ▒▓▓▓▓       ▓▓▓▓▓
+echo.   ▓▓▓▓              ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓        ▓▓▓▓
+echo.  ▓▓▓▓▓              ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▒       ▓▓▓▓░
+echo.
 
 :VERSION
 pnputil /? | findstr /C:"/enum-devices" >nul
-if %errorlevel% NEQ 0 GOTO UNSUPPORTED
+if %errorlevel% NEQ 0 goto :UNSUPPORTED
 
 :REGISTRY
 reg query "%BASE%" >nul 2>&1
-if %errorlevel% NEQ 0 goto REG_MISSING
-CALL :ASSIGN_STRING "%BASE%\MixerName" Spk SPEAKER
-CALL :ASSIGN_STRING "%BASE%\MixerName" Mic MICROPHONE
+if %errorlevel% NEQ 0 goto :REG_MISSING
+call :ASSIGN_STRING "%BASE%\MixerName" Spk SPEAKER
+call :ASSIGN_STRING "%BASE%\MixerName" Mic MICROPHONE
 for /f "delims=" %%a in ('reg query "%BASE%\MixerName" /f "" /k 2^>nul') do (
     echo %%a | findstr /b /i "%BASE%\MixerName" >nul
     if errorlevel 1 goto :END_LOOP
-    SET LANGUAGE=%%~na
-    CALL :ASSIGN_STRING "%%a" Spk SPEAKER
-    CALL :ASSIGN_STRING "%%a" Mic MICROPHONE
+    set LANGUAGE=%%~na
+    call :ASSIGN_STRING "%%a" Spk SPEAKER
+    call :ASSIGN_STRING "%%a" Mic MICROPHONE
 )
 :END_LOOP
 reg query "%BASE%\VID_041E&PID_3221" >nul 2>&1
-if %errorlevel% EQU 0 CALL :ASSIGN_STRING "%BASE%\VID_041E&PID_3221" MixerName MIXER
+if %errorlevel% EQU 0 call :ASSIGN_STRING "%BASE%\VID_041E&PID_3221" MixerName MIXER
 reg query "%BASE%\VID_041E&PID_322F" >nul 2>&1
-if %errorlevel% EQU 0 CALL :ASSIGN_STRING "%BASE%\VID_041E&PID_322F" MixerName MIXER
-if "%MIXER%" EQU "" goto MIXER_MISSING
+if %errorlevel% EQU 0 call :ASSIGN_STRING "%BASE%\VID_041E&PID_322F" MixerName MIXER
+if "%MIXER%" EQU "" goto :MIXER_MISSING
 
 :DEVICE
-FOR /F "skip=2 tokens=1* delims=:" %%a IN ('pnputil -enum-devices /connected /class "AudioEndpoint"') DO CALL :CHECK_ENDPOINTS "%%a" "%%b"
-IF "%RENDER_FOUND%" EQU "FALSE" GOTO RENDER_NOT_DETECTED
-IF "%CAPTURE_FOUND%" EQU "FALSE" GOTO CAPTURE_NOT_DETECTED
+for /F "skip=2 tokens=1* delims=:" %%a in ('pnputil -enum-devices /connected /class "AudioEndpoint"') do call :CHECK_ENDPOINTS "%%a" "%%b"
+if "%RENDER_FOUND%" EQU "FALSE" goto :RENDER_NOT_DETECTED
+if "%CAPTURE_FOUND%" EQU "FALSE" goto :CAPTURE_NOT_DETECTED
 
-GOTO SUCCESS
+goto :SUCCESS
 
 :CHECK_ENDPOINTS
-SET "VALUE=%~2"
-IF NOT "%VALUE:MMDEVAPI=%" EQU "%VALUE%" SET COUNT=1
-IF %COUNT% EQU 1 for /f "tokens=3 delims=\" %%m in ("%~2") do set "ID=%%m"
-IF %COUNT% EQU 2 for /f "tokens=*" %%m in ("%~2") do set "DESCRIPTION=%%m"
-IF %COUNT% EQU 3 (
-  IF "%DESCRIPTION%" EQU "%SPEAKER% (%MIXER%)" (
-    SET RENDER_FOUND=TRUE
+set "VALUE=%~2"
+if NOT "%VALUE:MMDEVAPI=%" EQU "%VALUE%" set COUNT=1
+if %COUNT% EQU 1 for /f "tokens=3 delims=\" %%m in ("%~2") do set "ID=%%m"
+if %COUNT% EQU 2 for /f "tokens=*" %%m in ("%~2") do set "DESCRIPTION=%%m"
+if %COUNT% EQU 3 (
+  if "%DESCRIPTION%" EQU "%SPEAKER% (%MIXER%)" (
+    set RENDER_FOUND=TRUE
     REG ADD "HKCU\SOFTWARE\Creative Tech\Audio Endpoint Selection\Sound Blaster Recon 3D Control Panel" /v Render /t REG_SZ /d "%ID%" /f >nul
   )
-  IF "%DESCRIPTION%" EQU "%MICROPHONE% (%MIXER%)" (
-    SET CAPTURE_FOUND=TRUE
+  if "%DESCRIPTION%" EQU "%MICROPHONE% (%MIXER%)" (
+    set CAPTURE_FOUND=TRUE
     REG ADD "HKCU\SOFTWARE\Creative Tech\Audio Endpoint Selection\Sound Blaster Recon 3D Control Panel" /v Capture /t REG_SZ /d "%ID%" /f >nul
   )
-  SET DESCRIPTION=""
-  SET ID=""
+  set DESCRIPTION=""
+  set ID=""
 )
-SET /a COUNT+=1
-GOTO :EOF
+set /a COUNT+=1
+goto :EOF
 
 :ASSIGN_STRING
 reg query %1 /v %2 >nul 2>&1
-if %errorlevel% NEQ 0 goto REG_MISSING
+if %errorlevel% NEQ 0 goto :REG_MISSING
 for /f "tokens=3*" %%a in ('reg query %1 /v %2 2^>nul') do (
     if "%%b"=="" (
         set "%3=%%a"
@@ -84,43 +84,43 @@ for /f "tokens=3*" %%a in ('reg query %1 /v %2 2^>nul') do (
         set "%3=%%a %%b"
     )
 )
-GOTO :EOF
+goto :EOF
 
 :RENDER_NOT_DETECTED
-ECHO.
-ECHO. Speaker not detected!
-PAUSE>nul
+echo.
+echo. Speaker not detected!
+pause>nul
 EXIT
 
 :CAPTURE_NOT_DETECTED
-ECHO.
-ECHO. Microphone not detected!
-PAUSE>nul
+echo.
+echo. Microphone not detected!
+pause>nul
 EXIT
 
 :REG_MISSING
-ECHO.
-ECHO. Registry entries not found!
-PAUSE>nul
+echo.
+echo. Registry entries not found!
+pause>nul
 EXIT
 
 :MIXER_MISSING
-ECHO.
-ECHO. Could not find sound mixer!
-PAUSE>nul
+echo.
+echo. Could not find sound mixer!
+pause>nul
 EXIT
 
 :UNSUPPORTED
-ECHO.
-ECHO. Windows 10 or above is required!
-PAUSE>nul
+echo.
+echo. Windows 10 or above is required!
+pause>nul
 EXIT
 
 :SUCCESS
-ECHO. Language: %LANGUAGE%
-ECHO. Mixer: %MIXER%
-ECHO. Speaker: %SPEAKER%
-ECHO. Microphone: %MICROPHONE%
-ECHO. Sound Blaster Recon 3D Control Panel configured Successfully
-PAUSE>nul
+echo. Language: %LANGUAGE%
+echo. Mixer: %MIXER%
+echo. Speaker: %SPEAKER%
+echo. Microphone: %MICROPHONE%
+echo. Sound Blaster Recon 3D Control Panel configured Successfully
+pause>nul
 exit
